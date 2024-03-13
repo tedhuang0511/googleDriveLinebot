a469d53c65f6ba1c35177f5270bdf84381593b96
TedHuang <sbbty218@gmail.com>
2024-03-13 17:58:02 +0800

    feat:Drive Service - 上傳檔案01

--- internal/app/service/drive/drive_service.go
+++ internal/app/service/drive/drive_service.go
@@ -2,7 +2,9 @@ package drive
 
 import (
 	"context"
+	domainDrive "firstProject/internal/domain/drive"
 	"golang.org/x/oauth2"
+	"io"
 	"log"
 )
 
@@ -93,4 +95,43 @@ func (dr *GoogleDriveService) ListSharedFolders(ctx context.Context, lineID stri
 	}
 
 	return result, nil
+}
+
+func (dr *GoogleDriveService) UploadFile(ctx context.Context, lineID string, fileName string, content io.ReadCloser) error {
+	dToken, err := dr.driveServiceDynamodb.GetGoogleOAuthToken(lineID)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	tok := oauth2.Token{
+		AccessToken:  dToken.AccessToken,
+		TokenType:    dToken.TokenType,
+		RefreshToken: dToken.RefreshToken,
+		Expiry:       dToken.Expiry,
+	}
+
+	_, err = dr.driveServiceGoogleOA.NewGoogleDrive(ctx, &tok)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	_, err = domainDrive.SaveContent(content)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	//log.Println("START Upload File To Drive")
+	//// 假設預設的儲存路徑
+	//folderID := "1kpLZfvk9XmSr4xtDvczAqYHIF8P3r8bk"
+	//err = d.UploadFile(folderID, fileName, file)
+	//if err != nil {
+	//	log.Println("err:", err)
+	//	return err
+	//}
+	return nil
+
 }
